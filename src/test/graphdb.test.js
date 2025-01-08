cae7d09d63920d039122bf17d3c6bd2fc9dfc252
Andrea Child <andrea.child@improving.com>
2025-01-07 21:56:26 -0800

    Refactored test functions for readability and reuse.

--- src/test/graphdb.test.js
+++ src/test/graphdb.test.js
@@ -2,64 +2,61 @@ import { graphDBInferenceSchema } from '../graphdb.js';
 import fs from "fs";
 
 test('node with same property and edge label should add underscore prefix', () => {
-    expect(graphDBInferenceSchema(readJSONFile('./src/test/node-edge-same-property-neptune-schema.json'), false)).toContain('_commonName:Commonname');
+    expect(graphDBInferenceSchema(readFile('./src/test/node-edge-same-property-neptune-schema.json'), false)).toContain('_commonName:Commonname');
 });
 
-test('should properly replace special chars in schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/special-chars-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/special-chars.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should properly replace special chars in schema', () => {
+    const actual = inferGraphQLSchema('./src/test/special-chars-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/special-chars.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output airport schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/airports-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/airports.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output airport schema', () => {
+    const actual = inferGraphQLSchema('./src/test/airports-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/airports.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output dining by friends schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/dining-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/dining.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output dining by friends schema', () => {
+    const actual = inferGraphQLSchema('./src/test/dining-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/dining.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output epl schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/epl-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/epl.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output epl schema', () => {
+    const actual = inferGraphQLSchema('./src/test/epl-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/epl.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output fraud graph schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/fraud-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/fraud.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output fraud graph schema', () => {
+    const actual = inferGraphQLSchema('./src/test/fraud-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/fraud.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output knowledge graph schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/knowledge-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/knowledge.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output knowledge graph schema', () => {
+    const actual = inferGraphQLSchema('./src/test/knowledge-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/knowledge.graphql')
+    expect(actual).toBe(expected);
 });
 
-test('should output security graph schema', function() {
-    let inferredSchema = graphDBInferenceSchema(readJSONFile('./src/test/security-neptune-schema.json'));
-    let fileSchema = readJSONFile('./src/test/security.graphql')
-    inferredSchema = sanitizeWhitespace(inferredSchema);
-    fileSchema = sanitizeWhitespace(fileSchema);
-    expect(inferredSchema).toBe(fileSchema);
+test('should output security graph schema', () => {
+    const actual = inferGraphQLSchema('./src/test/security-neptune-schema.json');
+    const expected = loadGraphQLSchema('./src/test/security.graphql')
+    expect(actual).toBe(expected);
 });
+
+function inferGraphQLSchema(neptuneSchemaFilePath) {
+    let neptuneSchema = readFile(neptuneSchemaFilePath);
+    let inferredSchema = graphDBInferenceSchema(neptuneSchema);
+    return sanitizeWhitespace(inferredSchema);
+}
+
+function loadGraphQLSchema(graphQLSchemaFilePath) {
+    let expectedSchema = readFile(graphQLSchemaFilePath);
+    return sanitizeWhitespace(expectedSchema);
+}
 
 function sanitizeWhitespace(str) {
     return str
@@ -67,6 +64,6 @@ function sanitizeWhitespace(str) {
         .trim();  // trim leading and trailing whitespace
 }
 
-function readJSONFile(fileName) {
+function readFile(fileName) {
     return fs.readFileSync(fileName, 'utf8');
 }
