84526a6bdea2eb9a4741c17874f2b15731d22924
kirill-stepanishin <kirill.stepanishin@improving.com>
2026-04-13 10:44:31 -0700

    Enable integration tests to run against IAM-enabled Neptune endpoints (#158)
    Integration tests for pipeline and CDK creation (Case05, Case06, Case07) fail against IAM-enabled Neptune databases because the test arguments don't include the required IAM flags. This commit introduces a shared helper and environment variable so the same test suite works against both IAM and non-IAM Neptune endpoints without changes to test configs.

--- test/TestCases/Case05/Case05.01.test.js
+++ test/TestCases/Case05/Case05.01.test.js
@@ -1,12 +1,12 @@
 
 import { jest } from '@jest/globals';
-import { readJSONFile } from '../../testLib';
+import { readJSONFile, getTestArgv } from '../../testLib';
 import { main } from "../../../src/main";
 
 const casetest = readJSONFile('./test/TestCases/Case05/case01.json');
 
 async function executeUtility() {    
-    process.argv = casetest.argv;
+    process.argv = getTestArgv(casetest);
     await main();
 }
 
--- test/TestCases/Case06/Case06.05.test.js
+++ test/TestCases/Case06/Case06.05.test.js
@@ -1,10 +1,10 @@
-import { readJSONFile } from '../../testLib';
+import { readJSONFile, getTestArgv } from '../../testLib';
 import { main } from "../../../src/main";
 
 const casetest = readJSONFile('./test/TestCases/Case06/cdk-pipeline-no-name.json');
 
 async function executeUtility() {    
-    process.argv = casetest.argv;
+    process.argv = getTestArgv(casetest);
     await main();
 }
 
